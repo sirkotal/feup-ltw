1aae1317920999c1bd0aeefeb70aaeedccf6d71b
sirkotal <lordkotal2002@gmail.com>
2023-04-09 13:25:26 +0100

    Created the changeAllArticleColors function

---
+++ js/selecting-elements/script.js
@@ -0,0 +1,9 @@
+function changeAllArticleColors() {
+    const articles = document.querySelectorAll("#products article");
+  
+    for (const article of articles) {
+      article.classList.add("sale");
+    }
+  }
+
+  changeAllArticleColors()
