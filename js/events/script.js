c4c71f5a9bc736f5d6e36d45d9a553994cc7df7f
sirkotal <lordkotal2002@gmail.com>
2023-04-09 13:47:45 +0100

    Created the attachBuyEvents function

---
+++ js/events/script.js
@@ -0,0 +1,12 @@
+function attachBuyEvents() {
+    const buttons = document.querySelectorAll("#products button");
+  
+    for (const button of buttons) {
+      button.addEventListener("click", function(event) {
+        console.log("BUY!");
+        console.log(event.currentTarget);
+      });
+    }
+}
+  
+attachBuyEvents()
